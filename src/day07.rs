4aee6ac3cb9faaee04d036dcdf99ed1ce71d2beb
Caleb Baum <caleb.baum@i-labs.tech>
2023-12-13 19:56:19 -0500

    Cool idea I found and learning new functions

--- src/day07.rs
+++ src/day07.rs
@@ -1,41 +1,39 @@
-use std::collections::BinaryHeap;
-
 use itertools::Itertools;
 const TEST_INPUT: &str = include_str!("../data/day07/test.txt");
 const REAL_INPUT: &str = include_str!("../data/day07/input.txt");
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct Hand {
-    cards: [char; 5],
-    bid: usize
-}
-
-impl Ord for Hand {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        get_hand_strength(self);
-        self.bid.cmp(&other.bid)
-    }
-}
-
-impl PartialOrd for Hand {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(&other))
-    }
-    
-}
-
-fn get_hand_strength(hand: &Hand) -> usize {
-    let card_by_count = hand.cards.iter().counts();
+fn get_hand_strength(cards: &str) -> (usize, usize) {
+    let card_by_count = cards.chars().counts();
     println!("{:?}", card_by_count  );
     let counts = card_by_count.values().collect_vec();
-
-    println!("{:?}", counts  );
-    let mut cards = hand.cards.to_vec();
-    let mut strength = 0;
-    let strength = cards.iter().map(|c| card_strength(*c)).collect_vec();
-    match(*counts.iter().max().unwrap_or(&0)) {
-        5 => 5
-    }
+    
+    // This is a cool idea, shift 4 bits for each card, then add the card strength thus a second rank sort
+    let idx = cards.chars().fold(0, |acc, c| {
+        let temp = (acc << 4) + card_strength(c);
+        println!("temp: {}", temp);
+        temp
+    });
+    println!("idx: {}", idx);
+    let hand_rank = match *counts.iter().max().unwrap_or(&&0_usize) {
+        5 => 6,
+        4 => 5,
+        3 => {
+            if counts.contains(&&2) {
+                4
+            } else {
+                3
+            }
+        },
+        2 => {
+            let pairs = counts.iter().filter(|c| *c == &&2).count();
+            match pairs {
+                2 => 2,
+                _ => 1,
+            }
+        },
+        _ => 0
+    };
+    (hand_rank, idx)
 }
 
 fn card_strength(card: char) -> usize {
@@ -51,17 +49,14 @@ fn card_strength(card: char) -> usize {
 
 pub fn solution() -> (usize, usize) {
 
-    let cards = TEST_INPUT.lines().map(|l| {
+    let mut cards = TEST_INPUT.lines().map(|l| {
         let (hand, bid) = l.split_once(" ").unwrap();
-        Hand {
-            cards: hand.chars().collect_vec().try_into().unwrap(),
-            bid: bid.parse().unwrap()
-        }
+        (bid.parse().unwrap(), get_hand_strength(hand))
     }).collect_vec();
-    let cards: BinaryHeap<Hand> = cards.into();
-    println!("vec deq {:?}", cards);
-    println!("top {:?}", cards.iter().next());
-    let part1 = 0;
+    cards.sort_unstable_by_key(|&(_, k)| k);
+    let part1 = cards.iter().enumerate().map(|(i, (bid, _))| {
+        bid * (i + 1)
+    }).sum::<usize>();
     let part2 = 0;
 
     (part1, part2)
